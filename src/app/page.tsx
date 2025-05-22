18407f7ccfa3abad914361c51510988eb3eef8f1
leonardomuniz <leocontato@outlook.com.br>
2025-05-22 08:52:41 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,73 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+enum GenderEnum {
+  female = 'female',
+  male = 'male',
+  other = 'other',
+}
+
+const schema = z.object({
+  firstName: z.string(),
+  gender: z.string(),
+  email: z.string().email(),
+  password: z.string().min(4),
+})
+
+type FormInput = z.infer<typeof schema>
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+    setError,
+  } = useForm<FormInput>({
+    defaultValues: {
+      firstName: 'Léo',
+      gender: GenderEnum.male,
+    },
+    resolver: zodResolver(schema),
+  })
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      console.log('chegou aqui')
+      throw new Error()
+    } catch (error) {
+      console.error(JSON.stringify(error))
+      setError('firstName', {
+        message: 'Escolhe oto nome',
+      })
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Primeiro nome: </label>
+      <input {...register('firstName')} type="text" />
+      <br />
+
+      <label>E-mail: </label>
+      <input {...register('email')} type="email" />
+      <br />
+
+      <label>Senha: </label>
+      <input {...register('password')} type="password" />
+      <br />
+      <label>Seleção de genero</label>
+      <select {...register('gender')} defaultValue="female">
+        <option value="female">female</option>
+        <option value="male">male</option>
+        <option value="other">other</option>
+      </select>
+      <br />
+      <button type="submit" disabled={isSubmitting}>
+        enviar
+      </button>
+    </form>
+  )
 }
